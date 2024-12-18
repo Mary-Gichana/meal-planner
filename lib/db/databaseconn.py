9d8b022294727efbc5e662328ddaa9b44732c4b0
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-18 14:47:37 +0300

    Initialize database connection and cursor for executing SQL commands

--- lib/db/databaseconn.py
+++ lib/db/databaseconn.py
@@ -6,3 +6,12 @@ def get_db_connection():
     conn = sqlite3.connect(DATABASE_PATH)
     conn.row_factory = sqlite3.Row
     return conn
+    
+
+def create_tables():
+    conn = get_db_connection()
+    Cursor = conn.cursor()
+
+    
+
+    
