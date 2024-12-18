d7f39610d1dfc83bebc14a20c1979283f5b1ecf3
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-18 21:27:37 +0300

    created created table

--- meal_planner.py
+++ meal_planner.py
@@ -2,3 +2,12 @@ import sqlite3
 
 CONN = sqlite3.connect('lib/db/mealplanner.db')
 CURSOR = CONN.cursor()
+
+CURSOR.execute("""
+    CREATE TABLE IF NOT EXISTS mealcategories(
+        id INTEGER PRIMARY KEY,
+        name TEXT)
+""")
+
+
+
